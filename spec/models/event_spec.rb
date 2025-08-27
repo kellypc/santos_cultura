5a71f8673e030d0922061315e23a263298844bda
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2025-08-27 20:54:58 -0300

    test: add model specs for Event validations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -8,6 +8,19 @@ RSpec.describe Event, type: :model do
 
   it "is invalid without a title" do
     event = build(:event, title: nil)
-    expect(event).to_not be_valid
+    expect(event).not_to be_valid
+    expect(event.errors[:title]).to include("can't be blank")
+  end
+
+  it "is invalid without a start_date" do
+    event = build(:event, start_date: nil)
+    expect(event).not_to be_valid
+    expect(event.errors[:start_date]).to include("can't be blank")
+  end
+
+  it "is invalid without an end_date" do
+    event = build(:event, end_date: nil)
+    expect(event).not_to be_valid
+    expect(event.errors[:end_date]).to include("can't be blank")
   end
 end
